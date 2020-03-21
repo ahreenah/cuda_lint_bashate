334d3fce7de88286c020e7d0989aa3d860947e11
medvosa <medvosa@yandex.ru>
2020-03-21 17:53:14 +0300

    fix extension bug

--- linter.py
+++ linter.py
@@ -1,7 +1,7 @@
-"""This module exports the Mypy plugin class."""
+"""This module exports the Bashate plugin class."""
 
 from cuda_lint import Linter, util
-
+import os
 
 class Bashate(Linter):
 
@@ -34,3 +34,16 @@ class Bashate(Linter):
         """Return the command line to execute."""
         result = self.executable + ' ' + self.base_cmd
         return result
+    
+    def tmpfile(self, cmd, code, suffix=''):
+        print('tmpfile',cmd,code,suffix)
+        """
+        Run an external executable using a temp file to pass code and return its output.
+        We override this to have the tmpfile extension match what is being
+        linted so E005 is valid.
+        """
+
+        filename, extension = os.path.splitext(self.filename)
+        extension = '.missingextension' if not extension else extension
+        return super().tmpfile(cmd, code, extension)
+    
